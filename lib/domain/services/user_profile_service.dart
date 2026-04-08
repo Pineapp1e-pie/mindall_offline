039016ee38a57dcce0e46b487ddb6250398731fb
Pineapp1e-pie <zemskovanb@gmail.com>
2026-04-08 16:37:55 +0400

    расширен UserProfileService (гендер, синхронизация)

--- lib/domain/services/user_profile_service.dart
+++ lib/domain/services/user_profile_service.dart
@@ -3,9 +3,13 @@ import '../models/user_profile.dart';
 
 class UserProfileService {
   static const _genderKey = 'user_gender';
+  static const _usernameKey = 'user_username';
   static const _lastPeriodKey = 'cycle_last_period';
   static const _cycleLengthKey = 'cycle_length';
   static const _periodDurationKey = 'cycle_period_duration';
+  static const _notifEnabledKey = 'notif_enabled';
+  static const _notifHourKey = 'notif_hour';
+  static const _notifMinuteKey = 'notif_minute';
 
   Future<UserProfile?> load() async {
     final prefs = await SharedPreferences.getInstance();
@@ -28,12 +32,21 @@ class UserProfileService {
       );
     }
 
-    return UserProfile(gender: gender, cycleSettings: cycleSettings);
+    return UserProfile(
+      gender: gender,
+      username: prefs.getString(_usernameKey),
+      cycleSettings: cycleSettings,
+    );
   }
 
   Future<void> saveGender(Gender gender) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(_genderKey, gender.name);
+  }
+
+  Future<void> saveUsername(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_usernameKey, username);
   }
 
   Future<void> saveCycleSettings(CycleSettings settings) async {
@@ -42,5 +55,28 @@ class UserProfileService {
         _lastPeriodKey, settings.lastPeriodStart.toIso8601String());
     await prefs.setInt(_cycleLengthKey, settings.cycleLengthDays);
     await prefs.setInt(_periodDurationKey, settings.periodDurationDays);
+  }
+
+  Future<bool> loadNotificationsEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_notifEnabledKey) ?? false;
+  }
+
+  Future<int> loadNotificationHour() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_notifHourKey) ?? 18;
+  }
+
+  Future<int> loadNotificationMinute() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_notifMinuteKey) ?? 0;
+  }
+
+  Future<void> saveNotificationSettings(
+      bool enabled, int hour, int minute) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_notifEnabledKey, enabled);
+    await prefs.setInt(_notifHourKey, hour);
+    await prefs.setInt(_notifMinuteKey, minute);
   }
 }
